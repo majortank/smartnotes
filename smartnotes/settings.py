71391a8886cbc3d12e9ed69c200942e2791fd5a6
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2024-05-06 14:49:39 +0200

    Define development mode Eenviroment varibale.

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+
 # ALLOWED_HOSTS = ['10.10.15.1','localhost', 'smartnotes.local']
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
