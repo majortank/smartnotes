aac309445f67ae53aaae76ee89653b0c7683fa43
Tankiso Thebe <thabo.tankiso.thebe@gmail.com>
2024-05-06 09:56:51 +0200

    Separate notes into shared and general notes

--- notes/views.py
+++ notes/views.py
@@ -17,8 +17,16 @@ class NotesListView(LoginRequiredMixin, ListView):
     template_name = 'notes/notes_list.html'
     login_url = '/login'
 
+    # def get_queryset(self):
+    #     return Notes.objects.filter(Q(user=self.request.user) | Q(shared_with=self.request.user))
     def get_queryset(self):
-        return Notes.objects.filter(Q(user=self.request.user) | Q(shared_with=self.request.user))
+        return self.request.user.notes.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['shared_notes'] = Notes.objects.filter(shared_with=self.request.user)
+        return context
+    
 
 class NotesDetailView(DetailView):
     model = Notes
